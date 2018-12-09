0695046ff0d669bbab40df1b196e21f0f3b02224
Maycon Seidel <maycon.seidel@gmail.com>
2018-12-09 01:12:23 -0200

    Self review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :sessions
-  resources :users
+  resources :sessions, only: :create
+  resources :users,    only: :create
 
   get 'signup', to: 'users#new',        as: 'signup'
   get 'login',  to: 'sessions#new',     as: 'login'
