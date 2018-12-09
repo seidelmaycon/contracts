c2f035525393c2326dff92aa1f9baa51775e96dc
Maycon Seidel <maycon.seidel@gmail.com>
2018-12-09 15:27:12 -0200

    Fix sign routes (#5)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :sessions, only: :create
-  resources :users,    only: :create
   resources :contracts, except: :delete
 
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'signin', to: 'sessions#new', as: 'signin'
+  get   'signup', to: 'users#new',       as: 'signup_form'
+  post  'signup', to: 'users#create',    as: 'signup'
+  get   'signin', to: 'sessions#new',    as: 'signin_form'
+  post  'signin', to: 'sessions#create', as: 'signin'
 
   root to: 'contracts#index'
 end
