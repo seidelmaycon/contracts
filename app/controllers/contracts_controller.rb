6e64eab03fbd33d009a89737c60da24ca2853073
Maycon Seidel <maycon.seidel@gmail.com>
2018-12-09 23:16:14 -0200

    Add contract create route

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
 class ContractsController < ApplicationController
+  before_action :autheticate_user
+
   def index
-    if logged_in?
-      @contracts = @current_user.contracts
+    @contracts = @current_user.contracts
+  end
+
+  def new
+    @contract = Contract.new
+  end
+
+  def create
+    @contract = Contract.new(contract_params.merge(user_id: @current_user.id))
+    if @contract.save
+      redirect_to contracts_path, notice: 'Your contract was added'
     else
-      redirect_to signin_path
+      render :new
     end
+  end
+
+  private
+
+  def autheticate_user
+    redirect_to signin_path unless logged_in?
+  end
+
+  def contract_params
+    params.require(:contract).permit(:ends_on, :costs, :vendor_id, :category_id)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
   describe '#create' do
     context 'with valid attributes' do
       let(:valid_attributes) do
@@ -15,7 +14,7 @@ RSpec.describe UsersController, type: :controller do
       it 'should create an user and redirect to root path' do
         expect do
           post :create, params: valid_attributes
-        end.to change{ User.count }.by(1)
+        end.to change { User.count }.by(1)
 
         expect(response).to redirect_to(root_path)
       end
@@ -31,7 +30,7 @@ RSpec.describe UsersController, type: :controller do
         it 'should not create an user' do
           expect do
             post :create, params: invalid_attributes
-          end.to change{ User.count }.by(0)
+          end.to change { User.count }.by(0)
 
           expect(response).to render_template(:new)
         end
