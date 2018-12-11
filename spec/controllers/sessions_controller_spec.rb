e8b9f93f946317b07b445a0784dc1b355ee397f7
Maycon Seidel <maycon.seidel@gmail.com>
2018-12-10 22:55:48 -0200

    Run rubocop -a (#8)

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-
   describe '#create' do
     let(:user) { FactoryBot.create(:user) }
 
