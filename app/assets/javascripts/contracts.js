0502bacfff0ee9eeee4338242d1947451d1aa469
Maycon Seidel <maycon.seidel@gmail.com>
2018-12-10 22:12:42 -0200

    Hide category select when no vendor

--- app/assets/javascripts/contracts.js
+++ app/assets/javascripts/contracts.js
@@ -6,6 +6,7 @@ jQuery(function() {
     vendor = $('#contract_vendor_id :selected').text();
     options = $(categories).filter("optgroup[label=" + vendor + "]").html();
     if (options) {
+      $("#category_field").removeClass("hidden");
       return $('#contract_category_id').html(options);
     } else {
       return $('#contract_category_id').empty();
