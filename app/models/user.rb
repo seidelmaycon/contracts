0f78846ec78ed2cf7f1f7e3781158088fc1e8238
Maycon Seidel <maycon.seidel@gmail.com>
2018-12-11 23:07:48 -0200

    Validate user email uniqueness (#10)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   validates :full_name, :email, :password_digest, presence: true
   validates :password, length: { minimum: 8 }
+  validates_uniqueness_of :email
 
   has_many :contracts
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_172633) do
+ActiveRecord::Schema.define(version: 2018_12_12_010249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_12_09_172633) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "vendors", force: :cascade do |t|
